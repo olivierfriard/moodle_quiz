b1a4f88afc9d6675da906938cd7a97f76e5f294e
Olivier Friard <olivier.friard@unito.it>
2024-10-24 10:30:21 +0200

    some changes

---
+++ quiz.py
@@ -0,0 +1,28 @@
+"""
+module of moodle_quiz app
+
+"""
+
+import random
+
+
+def get_quiz(question_data: dict, topic: str, step: str, n_questions: int, results: dict) -> list:
+    """
+    return a quiz (list of questions)
+
+    Args:
+
+        question_data (dict): all the question (extracted from moodle xml file)
+        topic (str): topic requested
+        step (str): step requested
+        n_questions (int): number of questions
+        results (dict): results of user
+
+    """
+
+    questions_list: list = []
+
+    for question_type in question_data[topic]:
+        questions_list.extend(question_data[topic][question_type])
+
+    return random.sample(questions_list, n_questions)
