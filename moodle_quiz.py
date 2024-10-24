0a5ebcc1dfa2340d6575719c913c7423ae9a05e1
Olivier Friard <olivier.friard@unito.it>
2024-10-24 17:01:07 +0200

    some changes

--- moodle_quiz.py
+++ moodle_quiz.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import re
+import quiz
+import json
 import tomllib
-from quart import Quart, jsonify, request, render_template
+import random
+from quart import Quart, jsonify, request, render_template, session, redirect
 
 
 def moodle_xml_to_dict_with_images(xml_file: str, base_category: str) -> dict:
@@ -131,8 +134,24 @@ for topic in question_data.keys():
     print()
 print()
 
+# check if file results.json exists
+flag_results_file_present = False
+results = {"questions": {}, "finished": {}}
+if Path("results.json").is_file():
+    try:
+        with open("results.json", "r") as file_in:
+            results = json.loads(file_in.read())
+        print("Results loaded:")
+        print(results)
+        flag_results_file_present = True
+    except Exception:
+        print("Error loading the results.json file")
+
+print(f"{results=}")
 
 app = Quart(__name__)
+
+app.secret_key = "votre_clé_secrète_sécurisée_ici"
 
 
 @app.route("/", methods=["GET"])
@@ -142,18 +161,92 @@ async def home():
 
 @app.route("/topic_list", methods=["GET"])
 async def topic_list():
-    return await render_template("topic_list.html")
+    # print(question_data.keys())
+
+    return await render_template("topic_list.html", topics=question_data.keys())
 
 
-# Route pour créer une nouvelle ressource via une requête POST
-@app.route("/data", methods=["POST"])
-async def create_data():
-    body = await request.get_json()
-    if "name" not in body:
-        return jsonify({"error": "Nom requis"}), 400
+@app.route("/view_topic/<topic>", methods=["GET"])
+async def view_topic(topic):
+    step_list = []
+    for i in range(1, config["STEP_NUMBER"] + 1):
+        if i == 1:
+            disabled = False
+        else:
+            disabled = i - 1 not in results["finished"].get(topic, [])
 
-    new_item = {"name": body["name"], "description": body.get("description", "Pas de description fournie.")}
-    return jsonify(new_item), 201
+        step_list.append({"name": f"Step #{i}", "idx": i, "disabled": disabled})
+    return await render_template("topic.html", topic=topic, step_list=step_list)
+
+
+@app.route("/view_quiz/<topic>/<step>", methods=["GET"])
+async def view_quiz(topic, step):
+    session["quiz"] = quiz.get_quiz(question_data, topic, step, config["N_STEP_QUESTIONS"], results)
+    session["quiz_position"] = 0
+    return redirect(f"/question/{topic}/{step}/0")
+
+
+@app.route("/question/<topic>/<step>/<int:idx>", methods=["GET"])
+async def question(topic, step, idx):
+    question = session["quiz"][idx]
+
+    answers = random.sample(question["answers"], len(question["answers"]))
+
+    return await render_template(
+        "question.html", question=question, answers=answers, topic=topic, step=step, idx=idx, total=len(session["quiz"])
+    )
+
+
+@app.route("/check_answer/<topic>/<step>/<int:idx>/<user_answer>", methods=["GET"])
+async def check_answer(topic, step, idx, user_answer):
+    def correct_answer():
+        if question["name"] not in results["questions"]:
+            results["questions"][question["name"]] = []
+        results["questions"][question["name"]].append(1)
+        return "You selected the correct answer."
+
+    def wrong_answer(correct_answer, feedback):
+        if question["name"] not in results["questions"]:
+            results["questions"][question["name"]] = []
+        results["questions"][question["name"]].append(0)
+        # feedback
+        if feedback:
+            return f"{feedback}\n\nThe correct answer is:\n\n{correct_answer}"
+        else:
+            return f"The correct answer is:\n\n{correct_answer}"
+
+    question = session["quiz"][idx]
+
+    # get user answer
+    if question["type"] in ("truefalse", "multichoice"):
+        user_answer = user_answer
+    elif question["type"] in ("shortanswer", "numerical"):
+        user_answer = "xx"
+    else:
+        print(f"Question type error: {question["type"]}")
+
+    # get correct answer
+    correct_answer_str: str = ""
+    feedback: str = ""
+    for answer in question["answers"]:
+        if answer["fraction"] == "100":
+            correct_answer_str = answer["text"]
+        if user_answer == answer["text"]:
+            feedback = answer["feedback"] if answer["feedback"] is not None else ""
+
+    text_output = [session["quiz"][idx]["questiontext"] + "\n\n"]
+    if user_answer.upper() == correct_answer_str.upper():
+        text_output.append(correct_answer())
+    else:
+        text_output.append(wrong_answer(correct_answer_str, feedback))
+
+    answers = random.sample(question["answers"], len(question["answers"]))
+
+    return "<br>".join(text_output)
+
+    return await render_template(
+        "question.html", question=question, answers=answers, topic=topic, step=step, idx=idx, total=len(session["quiz"])
+    )
 
 
 if __name__ == "__main__":
