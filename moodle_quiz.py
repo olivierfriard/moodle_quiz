3b7a5694977cd4617b7e15ab25d70e47796fb04a
Olivier Friard <olivier.friard@unito.it>
2024-10-27 20:59:48 +0100

    some changes

--- moodle_quiz.py
+++ moodle_quiz.py
@@ -1,14 +1,15 @@
 from pathlib import Path
+import hashlib
 import re
 import quiz
+import aiosqlite
 import json
 import tomllib
 import random
 from markupsafe import Markup
-from quart import Quart, render_template, session, redirect,request
+from quart import Quart, render_template, session, redirect, request, g, flash
 
 import moodle_xml
-
 
 
 xml_file = "data.xml"
@@ -21,7 +22,9 @@ if Path(xml_file).with_suffix(".txt").is_file():
 print(f"{config=}")
 
 # load questions from xml moodle file
-question_data = moodle_xml.moodle_xml_to_dict_with_images(xml_file, config["BASE_CATEGORY"], config["QUESTION_TYPES"])
+question_data = moodle_xml.moodle_xml_to_dict_with_images(
+    xml_file, config["BASE_CATEGORY"], config["QUESTION_TYPES"]
+)
 print()
 for topic in question_data.keys():
     print(topic)
@@ -49,6 +52,15 @@ app = Quart(__name__)
 app.config["DEBUG"] = True
 
 app.secret_key = "votre_clé_secrète_sécurisée_ici"
+
+DATABASE = "quiz.sqlite"
+
+
+async def get_db():
+    if "db" not in g:
+        g.db = await aiosqlite.connect(DATABASE)
+        g.db.row_factory = aiosqlite.Row
+    return g.db
 
 
 @app.route("/", methods=["GET"])
@@ -78,7 +90,9 @@ async def view_topic(topic):
 
 @app.route("/view_quiz/<topic>/<step>", methods=["GET"])
 async def view_quiz(topic, step):
-    session["quiz"] = quiz.get_quiz(question_data, topic, step, config["N_STEP_QUESTIONS"], results)
+    session["quiz"] = quiz.get_quiz(
+        question_data, topic, step, config["N_STEP_QUESTIONS"], results
+    )
     session["quiz_position"] = 0
     return redirect(f"/question/{topic}/{step}/0")
 
@@ -98,24 +112,31 @@ async def question(topic, step, idx):
 
     if question["type"] == "multichoice" or question["type"] == "truefalse":
         answers = random.sample(question["answers"], len(question["answers"]))
-        placeholder = 'Input a text'
-        type_='text'
+        placeholder = "Input a text"
+        type_ = "text"
     elif question["type"] in ("shortanswer", "numerical"):
-        answers =''
-        type_='number'
-        placeholder = 'Input a number' if question["type"] =="numerical" else "Input a text"
-
+        answers = ""
+        type_ = "number"
+        placeholder = (
+            "Input a number" if question["type"] == "numerical" else "Input a text"
+        )
 
     return await render_template(
-        "question.html", question=question, answers=answers,
+        "question.html",
+        question=question,
+        answers=answers,
         type_=type_,
         placeholder=placeholder,
-        topic=topic, step=step, idx=idx, total=len(session["quiz"])
+        topic=topic,
+        step=step,
+        idx=idx,
+        total=len(session["quiz"]),
     )
+
 
 @app.route("/check_answer/<topic>/<step>/<int:idx>/<user_answer>", methods=["GET"])
 @app.route("/check_answer/<topic>/<step>/<int:idx>", methods=["POST"])
-async def check_answer(topic, step, idx, user_answer:str=""):
+async def check_answer(topic, step, idx, user_answer: str = ""):
     def correct_answer():
         if question["name"] not in results["questions"]:
             results["questions"][question["name"]] = []
@@ -128,26 +149,26 @@ async def check_answer(topic, step, idx, user_answer:str=""):
         results["questions"][question["name"]].append(0)
         # feedback
         if answer_feedback:
-            return f"{answer_feedback}<br><br>The correct answer is:<br>{correct_answer}"
+            return (
+                f"{answer_feedback}<br><br>The correct answer is:<br>{correct_answer}"
+            )
         else:
             return f"The correct answer is:<br>{correct_answer}"
 
     question = session["quiz"][idx]
 
-    if request.method == 'GET':
-
+    if request.method == "GET":
         # get user answer
         if question["type"] in ("truefalse", "multichoice"):
             user_answer = user_answer
         else:
             print(f"Question type error: {question["type"]}")
 
-
-    if request.method == 'POST':
+    if request.method == "POST":
         # First, await request.form
         form_data = await request.form
         # Then, access form data with .get()
-        user_answer = form_data.get('user_answer')
+        user_answer = form_data.get("user_answer")
 
     print(f"{user_answer=} {type(user_answer)}")
 
@@ -159,8 +180,10 @@ async def check_answer(topic, step, idx, user_answer:str=""):
             correct_answer_str = answer["text"]
         print(f"{answer["text"]=}")
         if user_answer == answer["text"]:
-            print('ok')
-            answer_feedback = answer["feedback"] if answer["feedback"] is not None else ""
+            print("ok")
+            answer_feedback = (
+                answer["feedback"] if answer["feedback"] is not None else ""
+            )
 
     print(answer_feedback)
 
@@ -175,17 +198,84 @@ async def check_answer(topic, step, idx, user_answer:str=""):
     # answers = random.sample(question["answers"], len(question["answers"]))
 
     return await render_template(
-        "feedback.html", feedback=feedback, user_answer=user_answer, topic=topic, step=step, idx=idx, total=len(session["quiz"])
+        "feedback.html",
+        feedback=feedback,
+        user_answer=user_answer,
+        topic=topic,
+        step=step,
+        idx=idx,
+        total=len(session["quiz"]),
     )
 
-@app.route("/new_nickname", methods=["GET","POST"])
-async def new_nickname():
 
-    if request.method == 'GET':
+@app.route("/login", methods=["GET", "POST"])
+async def login():
+    if request.method == "GET":
+        return await render_template("login.html")
+    if request.method == "POST":
+        form_data = await request.form
+        password_hash = hashlib.sha256(form_data.get("password").encode()).hexdigest()
+        db = await get_db()
+        cursor = await db.execute(
+            "SELECT count(*) AS n_users FROM users WHERE nickname = ? AND password_hash = ?",
+            (
+                form_data.get("nickname"),
+                password_hash,
+            ),
+        )
+        n_users = await cursor.fetchone()
+        if not n_users[0]:
+            await flash("Incorrect login. Retry", "error")
+            return await render_template("login.html")
+
+        else:
+            session["nickname"] = form_data.get("nickname")
+            return redirect("/")
+
+
+@app.route("/new_nickname", methods=["GET", "POST"])
+async def new_nickname():
+    if request.method == "GET":
         return await render_template("new_nickname.html")
 
-    if request.method == 'POST':
-        pass
+    if request.method == "POST":
+        # First, await request.form
+        form_data = await request.form
+        # Then, access form data with .get()
+        nickname = form_data.get("nickname")
+        password1 = form_data.get("password1")
+        password2 = form_data.get("password2")
+
+        if not password1 or not password2:
+            await flash("A password is missing", "error")
+            return await render_template("new_nickname.html")
+
+        if password1 != password2:
+            await flash("Passwords are not the same", "error")
+            return await render_template("new_nickname.html")
+
+        password_hash = hashlib.sha256(password1.encode()).hexdigest()
+
+        db = await get_db()
+        cursor = await db.execute(
+            "SELECT count(*) AS n_users FROM users WHERE nickname = ?", (nickname,)
+        )
+        n_users = await cursor.fetchone()
+        print(n_users[0])
+        if n_users[0]:
+            flash("Nickname already taken", "error")
+            return await render_template("new_nickname.html")
+
+        try:
+            await db.execute(
+                "INSERT INTO users (nickname, password_hash) VALUES (?, ?)",
+                (nickname, password_hash),
+            )
+            await db.commit()
+            return "OK"
+        except aiosqlite.IntegrityError:
+            return "Error"
+
 
 if __name__ == "__main__":
     app.run()
