68a37aca9c46ccb349e1748e8f7cdb8baf890154
Olivier Friard <olivier.friard@unito.it>
2024-11-21 23:57:56 +0100

    some work

---
+++ aiken.py
@@ -0,0 +1,21 @@
+def aiken_to_dict_with_images(xml_file: str, question_types: list, image_files_path: str) -> dict:
+    """
+    Convert questions in aiken format to a dictionary
+
+    example:
+
+    $CATEGORY: Géographie Européenne
+    Quel pays est représenté sur cette carte ?
+    [Image: france.png]
+    A. Allemagne
+    # Feedback: ....
+    B. France
+    # Feedback: ....
+    C. Italie
+    # Feedback: ....
+    D. Espagne
+    # Feedback: ....
+    ANSWER: B
+    """
+
+    pass
